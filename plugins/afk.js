61b6ed5538a160c8aa36151b1fe12f77df0347c8
wahyupurna1245-spec <wahyupurna1245@gmail.com>
2026-08-08 23:33:12 +0700

    Create afk.js

---
+++ plugins/afk.js
@@ -0,0 +1,116 @@
+const fs = require('fs');
+const path = require('path');
+
+
+const afkFile =
+path.join(
+    __dirname,
+    '../database/afk.json'
+);
+
+
+
+function loadAFK() {
+
+    try {
+
+        if (!fs.existsSync(afkFile)) {
+            return {};
+        }
+
+        return JSON.parse(
+            fs.readFileSync(afkFile)
+        );
+
+    } catch {
+
+        return {};
+
+    }
+
+}
+
+
+
+function saveAFK(data) {
+
+    fs.writeFileSync(
+        afkFile,
+        JSON.stringify(
+            data,
+            null,
+            2
+        )
+    );
+
+}
+
+
+
+module.exports = {
+
+    command: ['afk'],
+    category: 'tools',
+    ownerOnly: false,
+
+
+    operate: async ({ sock, m, sender, args }) => {
+
+        try {
+
+
+            const afk =
+            loadAFK();
+
+
+            const alasan =
+            args.join(' ') ||
+            'Tidak ada alasan';
+
+
+
+            afk[sender] = {
+
+                alasan,
+
+                waktu:
+                Date.now()
+
+            };
+
+
+
+            saveAFK(afk);
+
+
+
+            await sock.sendMessage(
+                sender,
+                {
+                    text:
+`💤 *AFK AKTIF*
+
+Alasan:
+${alasan}
+
+Jika kamu mengirim pesan lagi,
+status AFK akan dihapus otomatis.`
+                },
+                {
+                    quoted:m
+                }
+            );
+
+
+        } catch(err) {
+
+            console.log(
+                'AFK Plugin Error:',
+                err
+            );
+
+        }
+
+    }
+
+};
