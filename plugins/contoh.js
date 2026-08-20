b1d981a7aa83e215640646a354c73f46d6527036
wahyupurna1245-spec <wahyupurna1245@gmail.com>
2026-08-20 11:17:18 +0700

    Delete plugins directory

--- plugins/contoh.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    command: ['contoh'], // Perintah yang memicu plugin ini
-    ownerOnly: false,          // True jika hanya bisa dipakai owner
-    operate: async ({ sock, m, sender, prefix, command }) => {
-        // Tulis logika atau balasan bot di sini
-        const teksBalasan = `Halo! Bot WhatsApp kamu aktif menggunakan sistem plugin.`;
-        await sock.sendMessage(sender, { text: teksBalasan }, { quoted: m });
-    }
-};
